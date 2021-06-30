7d25c18c1c3ce280cf682177ea17616a536430db
Debojyoti Roy <debojyotiroy245@gmail.com>
2021-06-30 22:02:19 +0530

    docs: added docs for each function

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-var readlineSync = require('readline-sync')
-var chalk = require('chalk')
+//libraries
+var readlineSync = require('readline-sync') //using to take input
+var chalk = require('chalk') //using to beautify the output
 
+// global variables
 var score = 0
 const wrong = chalk.hex('#cc3300');
 const right = chalk.greenBright;
 const quesColor = chalk.bold.hex('#66ff99')
 const gameDesc = chalk.hex('#00e699')
 
+//name of people who have high scores
 var highScores = [{
   name: 'Debojyoti',
   score: 5
 }]
 
+//Contains all the questions and answers
 var questions = [{
     question: 'Where do I live ? ',
     answer: 'Raiganj'
@@ -34,13 +38,14 @@ var questions = [{
   }
 ]
 
+//welcome user function....this function runs at the beginning
 function welcome() {
   console.log(chalk.bold.hex('#cc0066')('Hello'))
   var userName = readlineSync.question(chalk.hex('#ffcc00')('Who is this ? '))
   console.log('Welcome ' + chalk.hex('#00ccff')(userName) + chalk.hex('#cc6699')(' to DO YOU KNOW Debojyoti'))
   console.log()
 }
-
+//this function starts the quiz game
 function startGame() {
   console.log(gameDesc("This game is all about how much you know Debojyoti."))
   console.log(gameDesc("Just play it for fun. And enjoy!!!"))
@@ -54,7 +59,7 @@ function startGame() {
     return;
   }
 }
-
+//check if the user gets the correct answer and score the user
 function play(question, answer) {
   var userAnswer = readlineSync.question(quesColor(question))
 
@@ -68,13 +73,14 @@ function play(question, answer) {
   console.log(chalk.hex('#b300b3')('--------------------------'))
 }
 
+//iterate through the questions
 function game() {
   for (var i = 0; i < questions.length; i++) {
     var currentQuestion = questions[i];
     play(currentQuestion.question, currentQuestion.answer)
   }
 }
-
+// this function show the score of the user
 function showScores() {
   console.log(chalk.hex('#bbff33')('YAY,You scored: '), score)
   console.log(chalk.hex('#ff80bf')("Check out the high scores, if you should be there ping me and I'll update it"))
