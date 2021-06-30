174f066217c8fe8b9d0dad5875f91849348db436
Debojyoti Roy <debojyotiroy245@gmail.com>
2021-06-30 21:53:12 +0530

    fixed indentation

--- index.js
+++ index.js
@@ -7,15 +7,12 @@ const right = chalk.greenBright;
 const quesColor = chalk.bold.hex('#66ff99')
 const gameDesc = chalk.hex('#00e699')
 
-var highScores =[
-  {
-    name: 'Debojyoti',
-    score: 5
-  }
-]
+var highScores = [{
+  name: 'Debojyoti',
+  score: 5
+}]
 
-var questions = [
-  {
+var questions = [{
     question: 'Where do I live ? ',
     answer: 'Raiganj'
   },
@@ -37,55 +34,51 @@ var questions = [
   }
 ]
 
-function welcome(){
+function welcome() {
   console.log(chalk.bold.hex('#cc0066')('Hello'))
   var userName = readlineSync.question(chalk.hex('#ffcc00')('Who is this ? '))
   console.log('Welcome ' + chalk.hex('#00ccff')(userName) + chalk.hex('#cc6699')(' to DO YOU KNOW Debojyoti'))
   console.log()
 }
 
-function startGame(){
+function startGame() {
   console.log(gameDesc("This game is all about how much you know Debojyoti."))
   console.log(gameDesc("Just play it for fun. And enjoy!!!"))
   console.log()
   var startGameQues = readlineSync.question(chalk.hex('#ff1a66')('Do you want to start the game? y/n '))
-  if(startGameQues.toLowerCase()  == 'y'){
+  if (startGameQues.toLowerCase() == 'y') {
     game()
     showScores()
-  }
-  else{
+  } else {
     console.log(chalk.hex('#ffff4d')('Thank You!!'))
     return;
   }
 }
 
-function play(question , answer){
+function play(question, answer) {
   var userAnswer = readlineSync.question(quesColor(question))
 
-  if (userAnswer.toUpperCase() === answer.toUpperCase()){
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) {
     console.log(right('Right!'))
     score = score + 1
-  }
-  else{
+  } else {
     console.log(wrong('Wrong!'))
   }
-  console.log('Current Score: ',score)
+  console.log('Current Score: ', score)
   console.log(chalk.hex('#b300b3')('--------------------------'))
 }
 
-function game()
-{
-  for(var i = 0; i<questions.length;i++){
+function game() {
+  for (var i = 0; i < questions.length; i++) {
     var currentQuestion = questions[i];
-    play(currentQuestion.question,currentQuestion.answer)
+    play(currentQuestion.question, currentQuestion.answer)
   }
 }
 
-function showScores()
-{
-  console.log(chalk.hex('#bbff33')('YAY,You scored: '),score)
+function showScores() {
+  console.log(chalk.hex('#bbff33')('YAY,You scored: '), score)
   console.log(chalk.hex('#ff80bf')("Check out the high scores, if you should be there ping me and I'll update it"))
-  
+
   highScores.map(score => console.log(score.name, " : ", score.score))
 }
 
